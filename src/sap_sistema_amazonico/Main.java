9bf0543a8b7dd025f9e8d8a2aa7659b60b0bc850
14210 <14210@dg06e202.adm.bancoamazonia.sa>
2018-08-22 11:25:43 -0300

    commit

---
+++ src/sap_sistema_amazonico/Main.java
@@ -0,0 +1,12 @@
+
+package sap_sistema_amazonico;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+      Cadastro_Convenio cadastro =new Cadastro_Convenio();
+      cadastro.setVisible(true);
+    }
+    
+}
